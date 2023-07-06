557196245df348b8641171ba69cf79e681538c8e
Igor Dokai <igordokai@outlook.com>
2023-07-06 14:39:50 -0300

    comecando projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Pages
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    component: HomePageComponent
+  },
+  {
+    path: "main",
+    component: MainPageComponent
+  },
+  {
+    path: "**",
+    component: ErrorPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    MainPageComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
